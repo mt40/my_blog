8bbc5aa47fb92ed8d847a7008e073a8bf11d1b76
minhthai <minhthai40@gmail.com>
2019-01-12 15:09:27 +0800

    Add font-awesome icons

--- src/main/scala/common/Config.scala
+++ src/main/scala/common/Config.scala
@@ -5,6 +5,8 @@ import japgolly.scalajs.react.extra.router.BaseUrl
 object Config {
   val projectName = "my_blog"
 
+  val authorEmail = "minhthai40+blog@gmail.com"
+
   lazy val baseUrl: BaseUrl = {
     // Github Page serves "/<project_name>" not "/"
     val host = BaseUrl.fromWindowOrigin
